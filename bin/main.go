7c61cd59acb3309ff90009a82c7186fc625bb012
Rakibul Yeasin <ryeasin03@gmail.com>
2022-05-05 19:03:50 +0600

    Search test

--- bin/main.go
+++ bin/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/dreygur/bkashgo"
+	"github.com/dreygur/bkashgo/models"
 )
 
 var (
@@ -14,11 +15,112 @@ var (
 	isLiveStore = false
 )
 
+var (
+	createAgreementResponse *models.CreateAgreementResponse
+	executeAgreementReponse *models.ExecuteAgreementResponse
+	createPaymentResponse   *models.CreatePaymentResponse
+)
+
 func main() {
+	// bkash := bkashgo.GetBkash(username, password, appKey, appSecret, isLiveStore)
+	// token, err := bkash.GrantToken()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(token)
+	TestAgreement()
+}
+
+func TestAgreement() {
 	bkash := bkashgo.GetBkash(username, password, appKey, appSecret, isLiveStore)
+
 	token, err := bkash.GrantToken()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println(token)
+
+	if token == nil || len(token.IdToken) == 0 || len(token.RefreshToken) == 0 || token.StatusCode != "0000" {
+		log.Fatal(err)
+	}
+
+	func() {
+		req := &models.CreateRequest{
+			Mode:           "0000",
+			PayerReference: "dsfsodjf-w3y2sdjf83493-sdhfis",
+			CallbackURL:    "https://api.shikho.net/payment",
+			Currency:       "BDT",
+			Intent:         "Shikho Subscription",
+		}
+		resp, err := bkash.CreateAgreement(req, token)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if resp == nil || resp.StatusCode != "0000" {
+			log.Fatal("Invalid create agreement response")
+		}
+
+		createAgreementResponse = resp
+	}()
+
+	func() {
+		req := &models.ExecuteRequest{
+			PaymentID: createAgreementResponse.PaymentID,
+		}
+		resp, err := bkash.ExecuteAgreement(req, token)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if resp == nil || resp.StatusCode != "0000" {
+			log.Print(req)
+
+			log.Print(resp)
+			log.Fatal("Invalid execute agreement response")
+		}
+
+		executeAgreementReponse = resp
+	}()
+
+	func() {
+		req := &models.CreateRequest{
+			Mode:                    "0001",
+			PayerReference:          "01723888888",
+			CallbackURL:             "https://shikho.tech/payment",
+			AgreementID:             executeAgreementReponse.AgreementID,
+			Amount:                  "12",
+			Currency:                "BDT",
+			Intent:                  "sale",
+			MerchantInvoiceNumber:   "Inv0124",
+			MerchantAssociationInfo: "MI05MID54RF09123456One",
+		}
+		resp, err := bkash.CreatePayment(req, token)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if resp == nil || resp.StatusCode != "0000" {
+			log.Fatal("payment creattion failed")
+		}
+
+		createPaymentResponse = resp
+	}()
+
+	func() {
+		req := &models.ExecuteRequest{
+			PaymentID: createPaymentResponse.PaymentID,
+		}
+		resp, err := bkash.ExecutePayment(req, token)
+
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if resp == nil || resp.StatusCode != "0000" {
+			log.Fatal("payment creattion failed")
+		}
+	}()
 }
