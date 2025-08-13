decaac512a673e0cec32403cf553ed02d4f158e1
Rakibul Yeasin <ryeasin03@gmail.com>
2025-08-13 14:46:50 +0600

    Some updates

--- methods/debug.go
+++ methods/debug.go
@@ -5,12 +5,3 @@ func (b *Bkash) Debug(enable bool) *Bkash {
 	b.debug = enable
 	return b
 }
-
-func requireNonEmpty(fields ...string) bool {
-	for _, f := range fields {
-		if f == "" {
-			return false
-		}
-	}
-	return true
-}
